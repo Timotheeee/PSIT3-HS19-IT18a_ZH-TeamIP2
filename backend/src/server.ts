9164729d68eee96ee7b49facbd27f7ba7fb6af88
labortim <labortim@students.zhaw.ch>
2019-10-04 15:26:42 +0200

    added typescript to backend

---
+++ backend/src/server.ts
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+const express = require('express'),
+        bodyParser = require('body-parser'),
+        morgan = require('morgan'),
+        app = express();
+
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '\\..\\..\\frontend\\public'));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+var server = app.listen(process.env.PORT || 3000, listen);
+
+// This call back just tells us that the server has started
+function listen() {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('app listening at http://' + host + ':' + port);
+}
+
+const fs = require('fs');
+
+var db: any;
+load();
+
+function save() {
+    fs.writeFile("./db.json", JSON.stringify(db), function (err: any) {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
+function load() {
+    fs.readFile("./db.json", function (err: any, data: string) {
+        if (err) {
+            return;
+        }
+        try {
+            db = JSON.parse(data);
+        } catch (e) {
+
+        }
+    });
+}
+
+app.post('/api/', function (req: { body: { answer: any; }; }, res: { status: (arg0: number) => { json: (arg0: { data: string; }) => void; }; }) {
+    var answer = req.body.answer;
+    var data = answer == 0 ? "good" : "bad";
+
+    res.status(200).json({data:data});
+});
+
+app.get('/api/', function (req: any, res: { status: (arg0: number) => { json: (arg0: { db: any; }) => void; }; }) {
+    res.status(200).json({db: db});
+});
---
+++ frontend/src/components/QuestionBox.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h2>{{question}}</h2>
+    <div v-for="answer in possibleAnswers" :key=answer.id>
+      <input type="radio" name="answer" :value="answer.getId()" v-model="pickedAnswer" >
+      <label :for="answer.getId()">{{answer.answer}} </label>
+    </div>
+    <button @click="sendClicked">Submit</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+
+export default Vue.extend({
+    data() {
+      return {
+        question: '',
+        possibleAnswers: [] as Answer[],
+        pickedAnswer: ''
+      }
+    },
+    methods: {
+      sendClicked() {
+        console.log("send to JSON, value: " + this.pickedAnswer);
+      },
+    },
+    mounted() {
+      // fetch data json file
+      this.question = "How long did you sleep last night?";
+      this.possibleAnswers.push(new Answer(1, "below 6 hours"));
+      this.possibleAnswers.push(new Answer(2, "between 6 and 8 hours"));
+      this.possibleAnswers.push(new Answer(3, "over 8 hours"));
+    }
+});
+
+class Answer {
+  id: number;
+  answer: string;
+
+  constructor(id: number, answer: string) {
+    this.id = id;
+    this.answer = answer;
+  }
+
+  getId() : number {
+    return this.id;
+  }
+}
+
+</script>
+
