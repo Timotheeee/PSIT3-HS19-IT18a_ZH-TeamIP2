b17ba33872f200b050b5c7aeb0291e7e49713141
Lars Höhener <hoehelar@students.zhaw.ch>
2019-10-04 23:48:29 +0200

    Made getters for Question & Answer class and set data fields to private.

--- frontend/src/Answer.ts
+++ frontend/src/Answer.ts
@@ -1,9 +1,17 @@
-class Answer {
-  id: number;
-  answer: string;
+export class Answer {
+  private id: number;
+  private answer: string;
 
   constructor(id: number, answer: string) {
     this.id = id;
     this.answer = answer;
+  }
+
+  getId() : number {
+    return this.id;
+  }
+
+  getAnswer() : String {
+    return this.answer;
   }
 }
