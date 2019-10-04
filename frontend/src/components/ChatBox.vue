d7246c77041f5b7e5158cbe39c6df2d99374332c
Lars Höhener <hoehelar@students.zhaw.ch>
2019-10-04 23:55:07 +0200

    ChatBox.vue created, QuestionBox appropriately changed. Data in QuestionBox are now props. Data from the ChatBox are passed down as props to the QuestionBox.

---
+++ frontend/src/components/ChatBox.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <question-box :question="this.question.getQuestion()" :possibleAnswers="this.question.getPossibleAnswers()" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import {Question} from "./../Question";
+import {Answer} from "./../Answer"
+import QuestionBox from './QuestionBox.vue';
+
+
+export default Vue.extend({
+    data() {
+      return {
+         question: new Question(1, "How much did you sleep?")
+      }
+    },
+    created() {
+      this.question.addPossibleAnswer(new Answer(1, 'below 6 hours'));
+      this.question.addPossibleAnswer(new Answer(1, '6 to 8 hours'));
+      this.question.addPossibleAnswer(new Answer(1, 'over 8 hours'));
+      console.log("Question looks like: " + this.question);
+    },
+    components: {
+      QuestionBox
+    }
+});
+</script>
--- frontend/src/components/QuestionBox.vue
+++ frontend/src/components/QuestionBox.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <h2 id="question">{{question}}</h2>
-    <div v-for="answer in possibleAnswers" :key=answer.id>
-      <input type="radio" name="answer" :value="answer.getId()" v-model="pickedAnswer" >
-      <label :for="answer.getId()">{{answer.answer}} </label>
+    <div v-for="answer in possibleAnswers" :key=answer.getId()>
+      <input type="radio" name="answer" :value="answer.id" v-model="pickedAnswer" >
+      <label :for="answer.getId()">{{answer.getAnswer()}} </label>
     </div>
     <button @click="onButtonClick">Submit</button>
 
@@ -18,14 +18,23 @@
 
 <script lang="ts">
 import Vue from "vue";
+import {Answer} from "./../Answer";
 
 
 export default Vue.extend({
     data() {
       return {
-        question: '',
-        possibleAnswers: [] as Answer[],
         pickedAnswer: ''
+      }
+    },
+    props: {
+      question: {
+        type: String,
+        required: true
+      },
+      possibleAnswers: {
+        type: [],
+        required: true
       }
     },
     methods: {
@@ -34,26 +43,6 @@ export default Vue.extend({
       },
     },
     created() {
-      // fetch data json file
-      this.question = "How long did you sleep last night?";
-      this.possibleAnswers.push(new Answer(1, "below 6 hours"));
-      this.possibleAnswers.push(new Answer(2, "between 6 and 8 hours"));
-      this.possibleAnswers.push(new Answer(3, "over 8 hours"));
     }
 });
-
-class Answer {
-  id: number;
-  answer: string;
-
-  constructor(id: number, answer: string) {
-    this.id = id;
-    this.answer = answer;
-  }
-
-  getId() : number {
-    return this.id;
-  }
-}
-
 </script>
