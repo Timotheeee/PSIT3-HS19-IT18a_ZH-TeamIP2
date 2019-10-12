7997909b85e44c55f6a0a516d21515e1cb916bc0
labortim <labortim@students.zhaw.ch>
2019-10-12 11:51:02 +0200

    now using a file instead of mysql to prevent the server from crashing

--- backend/src/Dao.ts
+++ backend/src/Dao.ts
@@ -1,11 +1,21 @@
 const mysql = require('mysql');
+const fs = require('fs');
 
 export class Dao {
   private graph:string = "";
   private con:any;
 
   constructor(){
+    var this2 = this;
+    fs.readFile("./db.json", function (err: any, data: string) {
+      if (err) {
+        console.log("err: " + err);
+        return;
+      }
+      this2.graph = data;
+    });
 
+    return;
     this.con = mysql.createConnection({
       host: "timotheeee1.site",
       port: 3306,
@@ -14,14 +24,15 @@ export class Dao {
       database: "timothel_psit3"
     });
 
-    var this2 = this;
+
     this.con.connect(function (err: any) {
       if (err) throw err;
       console.log("Connected to db!");
       // this2.runQuery("use timothel_psit3");
       this2.con.query("SELECT * FROM graph", function (err: any, result: any) {
         if (err) throw err;
-        this2.graph = result[0].data
+        this2.graph = result[0].data;
+
       });
     });
   }
