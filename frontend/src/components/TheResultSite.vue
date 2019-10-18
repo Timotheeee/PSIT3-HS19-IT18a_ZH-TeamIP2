12686fdf4029c9df8ca7a3be3ca734579edebcbc
Lars Höhener <hoehelar@students.zhaw.ch>
2019-10-18 21:46:21 +0200

    Styled the ResultSite very basically. Added a score datafield and advice[] datafield.

--- frontend/src/components/TheResultSite.vue
+++ frontend/src/components/TheResultSite.vue
@@ -1,22 +1,15 @@
 <template>
-  <div class="container" style="background-color: white; border:1px solid black">
-    <div class="jumbotron text-center" style="background-color: white;">
-      <div>
-        <the-header />
-        <br />
-        <br />
-        <div id="resultsbox">
-          <div id="youare">You are...</div>
-          <br />
-          <img :src="circle" id="circle" />
-          <span id="hardworker">{{hardworker}}</span>
-          <span id="clueless">{{clueless}}</span>
-        </div>
-        <div id="tiltedredbox"></div>
-        <div id="suggestions">
-          <h3>Suggestions:</h3>
-          <b>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</b>
-        </div>
+   <div class="w-100 min-vh-100 background">
+    <div class="container p-0 shadow-lg">
+      <the-header />
+      <div class="jumbotron text-center jumbotron-fluid">
+          <h1 class="display-4">This is your score:</h1>
+          <h1 id="score" class="display-4"><b>{{ this.score }}</b></h1>
+
+          <h3>StudentScore's advices for you are:</h3>
+          <div v-for="advice in advices">
+            <p class="lead">{{advice}}</p>
+          </div>
       </div>
     </div>
   </div>
@@ -25,23 +18,35 @@
 <script lang="ts">
 import Vue from "vue";
 import TheHeader from "./TheHeader.vue";
+import ICountUp from 'vue-countup-v2'
 
 export default Vue.extend({
   data() {
     return {
-      logo: "./images/logo.png",
-      circle: "./images/circle.png",
-      hardworker: "40% Hard Worker",
-      clueless: "60% Clueless"
+      score: 70,
+      advices: [
+        "your such a bad student", "holy are you a bad student", "flextape can't fix your student habits"
+      ]
     };
   },
   components: {
     TheHeader
-  },
-  created() {}
+  }
 });
 </script>
 
 <style lang="scss">
-  @import "../css/styles";
+  @import "../css/colors";
+
+#jumbotron {
+    overflow-y: scroll;
+    height: calc(100vh - 100px);
+    display: grid;
+    background-color: $surface;
+  }
+
+#score {
+  margin-bottom: 60px;
+}
+
 </style>
