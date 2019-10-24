80c064f5bbe38b5768ca80a69ef6e06b5f0bb27c
Patric (Ryan) Steiger <steigpa1@students.zhaw.ch>
2019-10-24 20:20:01 +0200

    improve test gaming graph unit test.

--- frontend/src/model/MyGraphIterator.ts
+++ frontend/src/model/MyGraphIterator.ts
@@ -35,6 +35,4 @@ export class MyGraphIterator implements GraphIterator {
     isFinalNode(): boolean {
         throw new Error("Method not implemented.");
     }
-
-
 }
--- frontend/tests/model/GraphIterator.spec.ts
+++ frontend/tests/model/GraphIterator.spec.ts
@@ -1,10 +1,8 @@
-import { MyGraphIterator } from "../../src/MyGraphIterator";
-import { GraphIterator } from "../../src/MyGraphIterator";
+import { MyGraphIterator } from "../../src/model/MyGraphIterator";
 import { GraphFactory } from "../../src/model/GraphFactory";
 import { Graph } from "../../src/model/Graph";
 
-
-describe("MyGraphIterator", function() {    
+describe("MyGraphIterator", () => {    
     const minJSONGraph1Str = "graph = {\r\n  \"nodes\": [\r\n    {\r\n      \"id\": \"h\",\r\n      \"metadata\": {\r\n          \"isHead\": true,\r\n    \t  \"title\" : \"\"\r\n      }\r\n    },\r\n    {\r\n      \"id\": \"q1\",\r\n      \"metadata\": {\r\n    \t  \"title\" : \"Do you play video games?\"\r\n      }\r\n    },\r\n    {\r\n      \"id\": \"q2\",\r\n      \"metadata\": {\r\n    \t  \"title\" : \"Do you play League of Legends or World of Warcraft Classic?\"\r\n      }\r\n    },\r\n    {\r\n      \"id\": \"q3\",\r\n      \"metadata\": {\r\n    \t  \"title\" : \"Do you exercise at least twice a week?\"\r\n      }\r\n    },\r\n    {\r\n      \"id\": \"q4\",\r\n      \"metadata\": {\r\n    \t  \"title\" : \"Do you get more than 7 hours of sleep every night?\"\r\n      }\r\n    },\r\n    {\r\n      \"id\": \"fn\",\r\n      \"metadata\": {\r\n          \"isFinalNode\" : true,\r\n    \t  \"title\" : \"\"\r\n      }\r\n    }\r\n  ],\r\n  \"edges\": [\r\n    \/\/ Edge from H to Q1\r\n    {\r\n      \"source\": \"h\",\r\n      \"target\": \"q1\",\r\n    },\r\n    \/\/ Edges for Questions do you play video games\r\n    {\r\n      \"source\": \"q1\",\r\n      \"target\": \"q2\",\r\n      \"metadata\": {\r\n        \"answer\": \"Yes\",\r\n        \"score\": -10\r\n      }\r\n    },\r\n    {\r\n      \"source\": \"q1\",\r\n      \"target\": \"q3\",\r\n      \"metadata\": {\r\n        \"answer\": \"No\",\r\n        \"score\": 10\r\n      }\r\n    },\r\n    \/\/ Edges from Lol and Wow question    \r\n    {\r\n      \"source\": \"q2\",\r\n      \"target\": \"q4\",\r\n      \"metadata\": {\r\n        \"answer\": \"Yes, Riven is my Bae!\",\r\n        \"score\": -1000\r\n      }\r\n    },\r\n    {\r\n      \"source\": \"q2\",\r\n      \"target\": \"q4\",\r\n      \"metadata\": {\r\n        \"answer\": \"No, I play CS GO instead xD\",\r\n        \"score\": 1000\r\n      }\r\n    },\r\n    \/\/ Edges from the exercise question\r\n    {\r\n      \"source\": \"q3\",\r\n      \"target\": \"q4\",\r\n      \"metadata\": {\r\n        \"answer\": \"Yes, I am a true gym rat!\",\r\n        \"score\": 200\r\n      }\r\n    },\r\n    {\r\n      \"source\": \"q3\",\r\n      \"target\": \"q4\",\r\n      \"metadata\": {\r\n        \"answer\": \"No less than that\",\r\n        \"score\": 0\r\n      }\r\n    },\r\n    \/\/ From q4 to final node\r\n    {\r\n      \"source\": \"q4\",\r\n      \"target\": \"fn\",\r\n    }    \r\n  ]\r\n}\r\n";
     const q1Text = "Do you play video games?";
     const q2Text = "Do you play League of Legends or World of Warcraft Classic?";
@@ -21,7 +19,7 @@ describe("MyGraphIterator", function() {
     });
 
     describe("test gaming graph: left path", () =>{
-        it("should folllow the path and end on the final node.", () => {
+        it("should folllow the path and eventually arrive at the final node.", () => {
             let graphIterator = new MyGraphIterator(createMinimumViableGraph());
             graphIterator.getCurrentNode().getTitle().toBe(q1Text);
             graphIterator.choose("q2");
@@ -32,15 +30,16 @@ describe("MyGraphIterator", function() {
             graphIterator.getCurrentNode().question.toBe(q4Text);
             graphIterator.choose("fn");
 
-            graphIterator.getCurrentNode.IsFinalNode().toBe(true);                        
+            graphIterator.getCurrentNode.isFinalNode().toBe(true);                        
         });
     });
 
     describe("test non-gaming path: right path", () => {
-        it("should follow the path and end on the final node.", () => {
+        it("should follow the path and and eventually arrive at the final node", () => {
+            let graphIterator = new MyGraphIterator(createMinimumViableGraph());
+            graphIterator.getCurrentNode().getTitle().toBe(q1Text);
 
         });
     });
-
 
 });
