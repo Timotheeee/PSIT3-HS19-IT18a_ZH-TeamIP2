3d115530d87d481f3ebf904d4148ed02730d8b92
Lars Höhener <hoehelar@students.zhaw.ch>
2019-11-12 15:38:56 +0100

    Added tests to chatinteraction.

--- frontend/tests/components/ChatInteraction.spec.ts
+++ frontend/tests/components/ChatInteraction.spec.ts
@@ -1,6 +1,8 @@
 import { mount, shallowMount, Wrapper } from '@vue/test-utils'
 import Vue from 'vue';
 import ChatInteraction from './../../src/components/ChatInteraction.vue';
+import QuestionPack from './../../src/components/QuestionPack.vue';
+import ResultAnswer from './../../src/components/ResultAnswer.vue';
 
 
 describe("chatinteraction", () => {
@@ -18,5 +20,28 @@ describe("chatinteraction", () => {
     it ("has to contain a header", () => {
       expect(wrapper.html().includes("the-header")).toBeTruthy();
     })
+  })
+
+
+  describe("are events received", () => {
+
+    test("is processNextQuestion received and processQuestion called", () =>  {
+      let processQuestionStub = jest.fn();
+      wrapper.setMethods({ processQuestion: processQuestionStub()})
+      wrapper.find(QuestionPack).vm.$emit('processNextQuestion');
+      expect(processQuestionStub.mock.calls.length).toBe(1);
+    })
+
+    test("is submit received and onSubmit called", () =>  {
+      let onSubmitStub = jest.fn();
+      wrapper.setMethods({ onSubmit: onSubmitStub()})
+
+      let wrapperResultAnswer = shallowMount(ResultAnswer);
+      wrapperResultAnswer.vm.$emit('submit');
+
+      expect(onSubmitStub.mock.calls.length).toBe(1);
+    })
+
+
   })
 })
