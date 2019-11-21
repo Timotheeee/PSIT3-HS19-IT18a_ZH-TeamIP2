0e75ee060a38c776cba03b0a00478451ccfae46e
Erwin Tran <tranerw1@students.zhaw.ch>
2019-11-21 16:54:14 +0100

    setup uploading testfile

--- frontend/cypress/integration/questionnaire/ChatInteractionTest.ts
+++ frontend/cypress/integration/questionnaire/ChatInteractionTest.ts
@@ -1,6 +1,20 @@
-describe('', function() {
-  it('', function () {
-    expect(true).to.equal(true);
-    cy.visit(Cypress.config().baseUrl);
+describe('questionnaire integration test', function() {
+
+  describe('test username', function() {
+
+    it('should take placeholder username if user didnt give a name', function () {
+
+      const usernameGraph = new File(['data to upload'], cy.fixture("integration-test-graphs/username-graph.json"));
+
+      cy.fixture("integration-test-graphs/username-graph.json").then((content => {
+        cy.request('POST', 'localhost:3000/graph', content);
+      }));
+
+      cy.visit(Cypress.config().baseUrl + 'welcome');
+      cy.contains('Start StudentScore!').click();
+    })
+
+    it('should take username if user gave a name', function () {
+    })
   })
-});
+})
