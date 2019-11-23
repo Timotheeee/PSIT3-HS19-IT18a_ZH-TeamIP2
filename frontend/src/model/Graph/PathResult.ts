105f4e8fa95f9c83d974b928712cf6b249fca261
schlami3 <schlami3@students.zhaw.ch>
2019-11-23 21:00:42 +0100

    Cherry-pick Ryan's changes to PathResult

--- frontend/src/model/Graph/PathResult.ts
+++ frontend/src/model/Graph/PathResult.ts
@@ -1,18 +1,27 @@
+import { Node } from './Node';
 import { Edge } from './Edge';
 
 export class PathResult {
-    readonly _decision: Edge;
+    readonly _node: Node;
+    readonly _decision: Edge|null;
 
-    constructor(node: Node, decision: Edge|null) {        
+    constructor(node: Node, decision: Edge|null) {
+        this._node = node;
         this._decision = decision;
     }
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> Cherry-pick Ryan's changes to PathResult
     get node(): Node { return this._node;  }
     get decision(): Edge|null { return this._decision; }
 
 }
+<<<<<<< HEAD
 =======
     get decision(): Edge { return this._decision; }
 }
 >>>>>>> -change recommendation interface to use PathResult over GraphIterator
+=======
+>>>>>>> Cherry-pick Ryan's changes to PathResult
