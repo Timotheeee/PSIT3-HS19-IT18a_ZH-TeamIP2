9dc6cb41fe6743b32b4699cc1aee0e2aafefeaef
Erwin Tran <tranerw1@students.zhaw.ch>
2019-11-23 17:32:24 +0100

    setup a graph mock for integration tests

--- frontend/cypress/integration/questionnaire/ChatInteractionTest.ts
+++ frontend/cypress/integration/questionnaire/ChatInteractionTest.ts
@@ -2,16 +2,27 @@ describe('questionnaire integration test', function() {
 
   describe('test username', function() {
 
-    it('should take placeholder username if user didnt give a name', function () {
+    before(function() {
 
-      const usernameGraph = new File(['data to upload'], cy.fixture("integration-test-graphs/username-graph.json"));
+      // creates empty shell for the response
+      var result = {
+        success: true,
+        data: ""
+      };
 
+      // mocks the graph from the get request
+      cy.server();
       cy.fixture("integration-test-graphs/username-graph.json").then((content => {
-        cy.request('POST', 'localhost:3000/graph', content);
+        result.data = JSON.stringify(content);
+        cy.route('GET', '/graph', result);
       }));
+    })
+
+    it('should take placeholder username if user didnt give a name', function () {
 
       cy.visit(Cypress.config().baseUrl + 'welcome');
       cy.contains('Start StudentScore!').click();
+      cy.get('button.possible-answer').click();
     })
 
     it('should take username if user gave a name', function () {
