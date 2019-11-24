a552e5515b64bf779c981370db8a7ace53a21b8c
schlami3 <schlami3@students.zhaw.ch>
2019-11-24 17:24:20 +0100

    Implement RecommendationsGenerator classes
    * tsconfig.json: set target to "es2015" vs "es5" because of:
    * TS2569: Type 'IterableIterator<[string[], string]>' is not an array
    * type or a string type. Use compiler option '--downlevelIteration' to
    * allow iterating of iterators.
    *
    * tests for RecommendationsGenerators not finished
    * small indentation change in GraphFactory

--- frontend/src/model/Graph/Recommendations/ScoreBasedRecommendationsGenerator.ts
+++ frontend/src/model/Graph/Recommendations/ScoreBasedRecommendationsGenerator.ts
@@ -1,7 +1,9 @@
-import { RecommendationsGeneratorInterface } from "./RecommendationsGenerator";
-import { GraphIterator } from "../GraphIterator";
+import { RecommendationsGeneratorInterface } from './RecommendationsGenerator';
+import { GraphIterator } from '../GraphIterator';
 import { PathResult } from '../PathResult';
-import { Graph } from "../Graph";
+import { Graph } from '../Graph';
+import { GraphTools } from '../GraphTools';
+import { Edge } from '../Edge';
 
 export class ScoreBasedRecommendationsGenerator implements RecommendationsGeneratorInterface {
   private _json: string;
@@ -16,11 +18,66 @@ export class ScoreBasedRecommendationsGenerator implements RecommendationsGenera
 
   public generate(): string[] {
     // implement generate function
+    let scoreBasedRecommendations: ScoreBasedRecommendationsJSON = this.createScoreBasedRecommendationsFromJSON(this._json);
+    let numberOfPartitions: number = scoreBasedRecommendations.numberOfPartitions;
+    let recommendationsCatalogue: string[] = scoreBasedRecommendations.recommendations;
+    let userScore: number = this.calculateScore(this._path);
+    let recommendations: string[] = this.calculateRecommendations(numberOfPartitions, recommendationsCatalogue, userScore);
+    return recommendations;
+  }
 
-    return [""];
+  calculateScore(path: PathResult[]): number {
+    let score: number = 0;
+    if (path.length < 1) {
+      throw new Error("path is empty.");
+    }
+    for (let pathResult of path) {
+      let decision: Edge | null = pathResult.decision;
+      if (decision !== null) {
+        score += decision.weight;
+      }
+    }
+    return score;
+  }
+
+  createScoreBasedRecommendationsFromJSON(json: string): ScoreBasedRecommendationsJSON {
+    let scoreBasedRecommendations = JSON.parse(json);
+    return scoreBasedRecommendations;
+  }
+
+  /*
+   * Choose the appropriate score parition according to the student's score and
+   * return the respective recommendation string.
+   */
+  calculateRecommendations(numberOfPartitions: number, recommendationsCatalogue: string[], userScore: number): string[] {
+    let extrema: number[] = GraphTools.getExtremas(this._graph);
+    let minScore = extrema[0];
+    let maxScore = extrema[1];
+    let scoreRange = maxScore - minScore;
+    let partitionSize = scoreRange / numberOfPartitions;
+    let currentLowerPartitionBoundary: number = minScore;
+    let currentUpperPartitionBoundary: number = currentLowerPartitionBoundary + partitionSize;
+    let partitionCounter: number = 0;
+    let recommendations: string[] = [];
+
+    while (partitionCounter < numberOfPartitions) {
+      if (userScore <= currentUpperPartitionBoundary) {
+        recommendations.push(recommendationsCatalogue[partitionCounter]);
+        break;
+      }
+      currentLowerPartitionBoundary = currentUpperPartitionBoundary;
+      currentUpperPartitionBoundary += partitionSize;
+      partitionCounter += 1;
+    }
+    return recommendations;
   }
 
   public getScore(): number {
-    throw new Error('not yet implemented');
+    return this.calculateScore(this._path);
   }
+}
+
+export interface ScoreBasedRecommendationsJSON {
+  numberOfPartitions: number;
+  recommendations: string[];
 }
