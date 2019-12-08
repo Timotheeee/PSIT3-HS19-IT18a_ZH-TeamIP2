e7b4ff52c71a760e87b48c408960c98b21c90a13
Lars Höhener <hoehelar@students.zhaw.ch>
2019-12-08 16:31:12 +0100

    Included jsdoc in the package.json and documente the AxiosController, the GraphService and the LoginService.

--- frontend/src/services/AxiosController.ts
+++ frontend/src/services/AxiosController.ts
@@ -1,7 +1,15 @@
 import axios from 'axios';
-
+/**
+ * Class for transfering data between front- & backend with the axios-framework.
+ */
 export class AxiosController {
 
+  /**
+   * Makes a get request with the axios-framework
+   * @param {string} url - The url for the get request.
+   * @param url
+   * @returns A promise with the requested data.
+   */
   get(url: string): Promise<any> {
     return new Promise((resolve, reject) => {
       axios({
@@ -17,6 +25,12 @@ export class AxiosController {
     })
   }
 
+  /**
+   * Makes a post request with the axios-framework.
+   * @param {string} url - The url for the post request.
+   * @param {string} dataToPost - The data object which will be sent to the server
+   * @returns A promise if the post request was successfull.
+   */
   post(url: string, dataToPost: any): Promise<any> {
     return new Promise((resolve, reject) => {
       axios({
@@ -33,10 +47,19 @@ export class AxiosController {
     })
   }
 
+  /**
+   * Checks if a header is set in all requests.
+   * @param header - The to be checked header.
+   */
   public isHeaderSet(header: string): boolean {
     return axios.defaults.headers.common[header] ? true : false;
   }
 
+  /**
+   * Set a header in all requests.
+   * @param header - Key.
+   * @param value - Value.
+   */
   public setHeader(header: string, value:string) {
     axios.defaults.headers.common[header] = value;
   }
--- frontend/src/services/GraphService.ts
+++ frontend/src/services/GraphService.ts
@@ -2,14 +2,23 @@ import {AxiosController} from './AxiosController'
 import { Graph } from '../model/Graph/Graph';
 import { GraphFactory } from '../model/Graph/GraphFactory';
 
+/**
+ * Class for accessing & saving a graph to the backend
+ */
 export class GraphService {
   readonly url: string = '/graph';
   readonly axiosService: AxiosController;
+
 
   constructor() {
     this.axiosService = new AxiosController();
   }
 
+  /**
+   * Saves the graph in the database.
+   * @param {string} graphToPost - The graph which will be saved.
+   * @returns A boolean promise if the post request was successful.
+   */
   public async post(graphToPost: string): Promise<boolean> {
     const result = await this.axiosService.post(this.url, {graph: graphToPost});
 
@@ -20,6 +29,10 @@ export class GraphService {
     }
   }
 
+  /**
+   * Accesses the graph from the backend.
+   * @returns A promise with the graph object.
+   */
   public async get(): Promise<Graph> {
     const result = await this.axiosService.get(this.url);
 
@@ -31,6 +44,10 @@ export class GraphService {
     }
   }
 
+  /**
+   * Accesses the graph in the backend.
+   * @returns The graph in JSON-Format.
+   */
   public async getJSON(): Promise<string> {
     const result = await this.axiosService.get(this.url);
     return result.data;
